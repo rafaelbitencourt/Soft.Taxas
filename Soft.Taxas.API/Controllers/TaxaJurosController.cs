307506279e04f27ec0886351d5c34dffd9038f00
rafaelbitencourt <rafael_silbit@hotmail.com>
2021-08-19 12:59:15 -0300

    Criado a API

---
+++ Soft.Taxas.API/Controllers/TaxaJurosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Soft.Taxas.Dominio;
+
+namespace Soft.Taxas.API.Controllers
+{
+    [ApiController]
+    [Route("taxajuros")]
+    public class TaxaJurosController : ControllerBase
+    {
+        private readonly ITaxasService _taxasService;
+
+        public TaxaJurosController(ITaxasService taxasService)
+        {
+            _taxasService = taxasService;
+        }
+
+        [HttpGet]
+        public string RecuperarTaxaDeJuros() => 
+            _taxasService.RecuperarTaxaDeJuros().ToString();
+    }
+}
